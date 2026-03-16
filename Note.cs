5d31d2a27abc21a5f03e7993be20c74a1a3ec164
harunya0 <168539437+harunya0@users.noreply.github.com>
2026-03-16 16:31:36 +0900

    DBの前提クラス

---
+++ Note.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Text.RegularExpressions;
+
+namespace SmartNote
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
